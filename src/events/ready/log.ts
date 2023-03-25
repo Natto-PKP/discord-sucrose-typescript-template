27fd5e1a2cf064b5aa7b12efd6c84e646499abca
Nat <wmainvielle@gmail.com>
2023-03-25 14:42:17 +0100

    chore: update discord-sucrose to 6.4.4

--- src/events/ready/log.ts
+++ src/events/ready/log.ts
@@ -1,8 +1,9 @@
+import { Logger as SucroseLogger } from 'discord-sucrose';
 import type { EventHandlerParams } from 'discord-sucrose';
 import Logger from '../../services/Logger';
 
 export default async ({ sucrose }: EventHandlerParams<'ready'>) => {
-  Logger.write("I'm connected !");
+  Logger.write(SucroseLogger.style("I'm connected", 'rainbow'));
 
   await sucrose.commands.define('eval');
 };
