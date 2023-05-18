6990a28a756e099d4254f78d72cd239668ecab43
Nat <wmainvielle@gmail.com>
2023-05-18 10:59:25 +0200

    chore: Update for 7.0.1

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { Sucrose } from 'discord-sucrose';
 import { GatewayIntentBits, Partials } from 'discord.js';
 import dotenv from 'dotenv';
 
+import Cooldown from './services/Cooldown';
+
 dotenv.config();
 
 Sucrose.build({
   env: { ext: 'ts', source: 'src' },
   intents: [GatewayIntentBits.Guilds],
   partials: [Partials.Channel],
+  cooldown: Cooldown,
 });
--- src/services/Cooldown.ts
+++ src/services/Cooldown.ts
@@ -1,3 +1,19 @@
-import { CooldownManager } from 'discord-sucrose';
+import { BaseCooldownManager, CooldownMethodParams, CooldownValue } from 'discord-sucrose';
 
-export default new CooldownManager();
+const cache = new Map<string, CooldownValue>();
+
+class Cooldown extends BaseCooldownManager<typeof cache> {
+  constructor() {
+    super(cache);
+  }
+
+  public override async get({ key }: CooldownMethodParams) {
+    return this.cache.get(key);
+  }
+
+  public override async set({ key, value, stack }: CooldownMethodParams & CooldownValue) {
+    this.cache.set(key, { value, stack });
+  }
+}
+
+export default new Cooldown();
